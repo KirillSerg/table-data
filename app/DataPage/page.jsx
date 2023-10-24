8b4dbcea1f763cd4a984072713a858c1fc25b7e5
KirillSerg <kirill.sergienko1989@gmail.com>
2023-10-24 23:48:37 +0300

    feat: add render some styled table

--- app/DataPage/page.jsx
+++ app/DataPage/page.jsx
@@ -5,23 +5,63 @@ import service from "../(service)/service";
 
 const DataPage = () => {
   const [data, setData] = useState(null);
-  console.log(data);
+
   useEffect(() => {
     (async () => {
       const tableResponse = await service.getData();
       if (tableResponse) {
-        console.log(tableResponse);
         setData(tableResponse);
       }
     })();
   }, []);
 
   return (
-    <section>
-      {data &&
-        data.results.map((row) => {
-          return <p key={row.id}>{row.name}</p>;
-        })}
+    <section className="">
+      <table className="w-full ">
+        <thead>
+          <tr>
+            <th className="sticky top-0 bg-neutral-600 text-white border-solid border-2 p-1">
+              №
+            </th>
+            {data &&
+              Object.keys(data.results[0]).map((th, id) => {
+                return (
+                  <th
+                    className="sticky top-0 bg-neutral-600 text-white border-solid border-2 p-1"
+                    key={id}
+                  >
+                    {th}
+                  </th>
+                );
+              })}
+          </tr>
+        </thead>
+
+        <tbody className="">
+          {data &&
+            data.results.map((row, id) => {
+              return (
+                <tr key={row.id}>
+                  <td>
+                    <textarea
+                      disabled
+                      className="w-full border-solid border-2 p-1 resize-none"
+                      defaultValue={id + 1}
+                    />
+                  </td>
+                  {Object.values(row).map((td, id) => (
+                    <td key={id}>
+                      <textarea
+                        className="w-full border-solid border-2 p-1 resize-none"
+                        defaultValue={td}
+                      />
+                    </td>
+                  ))}
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
     </section>
   );
 };
